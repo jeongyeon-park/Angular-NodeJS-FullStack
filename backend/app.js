eb0bd944590b64bb40bfc7c55075debc291da46f
jeongyeon <dus7653@gmail.com>
2022-05-19 17:08:59 -0500

    ADD: REST API

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use((req, res, next)=>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Orogin, X-Requested-With, Content-Type, Accept');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,POST,PATCH,DELETE,OPTIONS'
+  );
+  next();
+})
+
+app.post("/api/posts", (req,res,next)=>{
+  const post = req.body;
+  console.log(post);
+
+  return res.status(201).json({
+    message: 'Post added successfully'
+  });
+});
+
+
+app.get('/api/posts',(req, res, next)=>{
+  const posts = [
+    {
+      id : 'fadf1234a',
+      title: 'First server-side post',
+      content: 'This is commin from server'
+    },
+    {
+      id : 'fadf1234ll',
+      title: 'Second server-side post',
+      content: 'This is commin from server'
+    },
+  ];
+  return res.status(200).json({
+    message: 'Posts fetched succesfully!',
+    posts:posts
+  });
+});
+
+module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http'
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 
@@ -12,6 +14,8 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostsService } from './posts/posts.service';
+
 
 @NgModule({
   declarations: [
@@ -28,9 +32,10 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
